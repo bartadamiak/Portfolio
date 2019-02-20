4cf4d864a33c64cfc810092e5a039eb70404c956
bartadam <bart.adamiak@gmail.com>
2019-02-20 15:56:32 +0100

    Logos

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-let portfolioButton = document.querySelector('.header-menu-portfolio');
-let contactButton = document.querySelector('.header-menu-contact');
-let arrowButton = document.querySelector('.fa-arrow-alt-circle-down');
+// let portfolioButton = document.querySelector('.header-menu-portfolio');
+// let contactButton = document.querySelector('.header-menu-contact');
+// let arrowButton = document.querySelector('.fa-arrow-alt-circle-down');
 
 
-portfolioButton.addEventListener('click', function(e) {
-    e.preventDefault;
-    window.location.hash = '#portfolio';
-    // if (window.location.hash = '#portfolio' ) {
-    //     window.location.hash = '#portfolio'
-    // };
+// portfolioButton.addEventListener('click', function(e) {
+//     e.preventDefault;
+//     window.location.hash = '#portfolio';
+//     if (window.location.hash = '#portfolio' ) {
+//         window.location.hash = '#portfolio'
+//     };
    
 
-});
+// });
 
-contactButton.addEventListener('click', function(e) {
-    e.preventDefault;
-    window.location.hash = '#contact';
-    // if (window.location.hash = '#contact') {
-    //     window.location.hash = '#contact'
-    // }
-});
+// contactButton.addEventListener('click', function(e) {
+//     e.preventDefault;
+//     window.location.hash = '#contact';
+//     // if (window.location.hash = '#contact') {
+//     //     window.location.hash = '#contact'
+//     // }
+// });
 
-arrowButton.addEventListener('click', function(e) {
-    e.preventDefault;
-    window.location.hash = '#portfolio';
-    // if (window.location.hash = '#portfolio' && scrollY) {
-    //     window.location.hash = '#portfolio'
-    // }
-})
+// arrowButton.addEventListener('click', function(e) {
+//     e.preventDefault;
+//     window.location.hash = '#portfolio';
+//     // if (window.location.hash = '#portfolio' && scrollY) {
+//     //     window.location.hash = '#portfolio'
+//     // }
+// })
